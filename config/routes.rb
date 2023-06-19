527b5ce7148db9b3b255b6c3a7e01e90b5bb3469
digiappdev <syedmdehsan@yahoo.com>
2023-06-19 23:57:12 +0100

    add single post page and refactor

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :articles, only: [:show]
+
   namespace :admin do 
     resources :categories
     devise_for :users
