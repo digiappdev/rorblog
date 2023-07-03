f96621468b2fa29332369bcdc649219e1ba367a4
digiappdev <syedmdehsan@yahoo.com>
2023-07-03 02:20:19 +0100

    add username and user bio to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
+  attr_writer :login
+
+  def login
+    @login || self.username || self.email
+  end
+
   has_many :comments
   has_many :categories
   has_many :articles
